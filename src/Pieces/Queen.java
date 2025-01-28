f722323e55f6c5e2127c03aa0b7f4cd9c0322f31
Tobias Hegge <tobiashegge05@gmail.com>
2025-01-28 16:54:03 +0100

    Fixed legalTiles check for Queen class

--- src/Pieces/Queen.java
+++ src/Pieces/Queen.java
@@ -34,14 +34,42 @@ public class Queen extends Piece{
 
     @Override
     public ArrayList<Point> getLegalTiles(Piece[][] boardPieces){
-        ArrayList<Point> legalTiles = new ArrayList<Point>();
-
-        for (int i = 0; i < boardPieces.length; i++){
-            for (int j = 0; j < boardPieces[i].length; j++){
-                legalTiles.add(new Point(i, j));
+        ArrayList<Point> legalTiles = new ArrayList<>();
+        int x = getPos().x;
+        int y = getPos().y;
+        int rows = boardPieces.length;
+        int cols = boardPieces[0].length;
+    
+        int[] directions = {-1, 0, 1};
+    
+        for (int i : directions) {
+            for (int j : directions) {
+                if (i == 0 && j == 0) continue;
+    
+                int newX = x;
+                int newY = y;
+    
+                while (true) {
+                    newX += j;
+                    newY += i;
+    
+                    if (newX >= 0 && newX < cols && newY >= 0 && newY < rows) {
+                        Piece targetPiece = boardPieces[newY][newX];
+                        if (targetPiece == null) {
+                            legalTiles.add(new Point(newX, newY));
+                        } else {
+                            if (targetPiece.getColor() != getColor()) {
+                                legalTiles.add(new Point(newX, newY));
+                            }
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
             }
         }
-
+    
         return legalTiles;
     }
 }
