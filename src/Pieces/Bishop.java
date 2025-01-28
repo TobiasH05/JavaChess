157f90185ad31f5a22f22c47487f7f570792fa87
Tobias Hegge <tobiashegge05@gmail.com>
2025-01-28 16:48:07 +0100

    Fixed legalTiles check for Bishop class

--- src/Pieces/Bishop.java
+++ src/Pieces/Bishop.java
@@ -35,14 +35,60 @@ public class Bishop extends Piece{
 
     @Override
     public ArrayList<Point> getLegalTiles(Piece[][] boardPieces){
-        ArrayList<Point> legalTiles = new ArrayList<Point>();
-
-        for (int i = 0; i < boardPieces.length; i++){
-            for (int j = 0; j < boardPieces[i].length; j++){
-                legalTiles.add(new Point(i, j));
+        ArrayList<Point> legalTiles = new ArrayList<>();
+        int x = getPos().x;
+        int y = getPos().y;
+        int rows = boardPieces.length;
+        int cols = boardPieces[0].length;
+    
+        //check up-right
+        for (int i = 1; x + i < cols && y - i >= 0; i++) {
+            if (boardPieces[y - i][x + i] != null) {
+                if (boardPieces[y - i][x + i].getColor() != getColor()) {
+                    legalTiles.add(new Point(x + i, y - i));
+                }
+                break;
+            } else {
+                legalTiles.add(new Point(x + i, y - i));
             }
         }
-
+    
+        //check up-left
+        for (int i = 1; x - i >= 0 && y - i >= 0; i++) {
+            if (boardPieces[y - i][x - i] != null) {
+                if (boardPieces[y - i][x - i].getColor() != getColor()) {
+                    legalTiles.add(new Point(x - i, y - i));
+                }
+                break;
+            } else {
+                legalTiles.add(new Point(x - i, y - i));
+            }
+        }
+    
+        //check down-right
+        for (int i = 1; x + i < cols && y + i < rows; i++) {
+            if (boardPieces[y + i][x + i] != null) {
+                if (boardPieces[y + i][x + i].getColor() != getColor()) {
+                    legalTiles.add(new Point(x + i, y + i));
+                }
+                break;
+            } else {
+                legalTiles.add(new Point(x + i, y + i));
+            }
+        }
+    
+        //check down-left
+        for (int i = 1; x - i >= 0 && y + i < rows; i++) {
+            if (boardPieces[y + i][x - i] != null) {
+                if (boardPieces[y + i][x - i].getColor() != getColor()) {
+                    legalTiles.add(new Point(x - i, y + i));
+                }
+                break;
+            } else {
+                legalTiles.add(new Point(x - i, y + i));
+            }
+        }
+    
         return legalTiles;
     }
 }
