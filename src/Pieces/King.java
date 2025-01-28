f463b3034da3de5c24bcbeb1a1bdfa40da3cde2b
Tobias Hegge <tobiashegge05@gmail.com>
2025-01-28 16:50:28 +0100

    Fixed legalTiles check for King class

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -34,14 +34,28 @@ public class King extends Piece{
 
     @Override
     public ArrayList<Point> getLegalTiles(Piece[][] boardPieces){
-        ArrayList<Point> legalTiles = new ArrayList<Point>();
-
-        for (int i = 0; i < boardPieces.length; i++){
-            for (int j = 0; j < boardPieces[i].length; j++){
-                legalTiles.add(new Point(i, j));
+        ArrayList<Point> legalTiles = new ArrayList<>();
+        int x = getPos().x;
+        int y = getPos().y;
+        int rows = boardPieces.length;
+        int cols = boardPieces[0].length;
+    
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;  // Skip the current position
+    
+                int newX = x + j;
+                int newY = y + i;
+    
+                if (newX >= 0 && newX < cols && newY >= 0 && newY < rows) {
+                    Piece targetPiece = boardPieces[newY][newX];
+                    if (targetPiece == null || targetPiece.getColor() != getColor()) {
+                        legalTiles.add(new Point(newX, newY));
+                    }
+                }
             }
         }
-
+    
         return legalTiles;
     }
 }
